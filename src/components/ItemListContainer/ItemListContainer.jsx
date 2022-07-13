c345f234a5628302a33edbb3291556d62ad1a000
NicolasDeminas <73315878+NicolasDeminas@users.noreply.github.com>
2022-07-13 20:53:44 -0300

    Item y ItemList

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./ItemListContainer.css";
 import ItemList from "../ItemList/ItemList";
+import arr from "../../mockData";
 
 const ItemListContainer = ({ greeting }) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData(arr);
+    }, 3000);
+  });
+
   return (
     <div>
       <h1>{greeting}</h1>
-      <ItemList />
+      <ItemList data={data} />
     </div>
   );
 };
---
+++ src/mockData.js
@@ -0,0 +1,40 @@
+const arr = [
+  {
+    id: 1,
+    title: "Coca-Cola",
+    description: "Bebida sin alcohol",
+    price: 150,
+    pictureUrl:
+      "https://jumboargentina.vtexassets.com/arquivos/ids/441468/Coca-Cola-25-L-3-17483.jpg?v=636528846231600000",
+    stock: 10,
+  },
+  {
+    id: 2,
+    title: "Sprite",
+    description: "Bebida sin alcohol",
+    price: 150,
+    pictureUrl:
+      "https://jumboargentina.vtexassets.com/arquivos/ids/682853/Gaseosa-Sprite-Lima-lim-n-1-5-L-2-248096.jpg?v=637764969686070000",
+    stock: 10,
+  },
+  {
+    id: 3,
+    title: "Fanta",
+    description: "Bebida sin alcohol",
+    price: 150,
+    pictureUrl:
+      "https://depotexpress.com.ar/tienda/wp-content/uploads/2020/05/FANTA-NARANJA-X-1.5L.png",
+    stock: 10,
+  },
+  {
+    id: 4,
+    title: "Cerveza Quilmes",
+    description: "Bebida con alcohol",
+    price: 150,
+    pictureUrl:
+      "https://pulperiaquilapan.com/wp-content/uploads/2014/08/quilmes.jpg",
+    stock: 10,
+  },
+];
+
+export default arr;
